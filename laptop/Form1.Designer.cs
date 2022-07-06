76a696356b12986d538b3c47d9ee697281da98b3
Vesela Apostolova <100559743+vap0204@users.noreply.github.com>
2022-07-07 01:16:37 +0300

    Add files via upload

---
+++ laptop/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿
+namespace _2101681070_VeselaApostolova_domashna_rabota.Zad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblComputerName = new System.Windows.Forms.Label();
+            this.cbWindows = new System.Windows.Forms.CheckBox();
+            this.cbBackpack = new System.Windows.Forms.CheckBox();
+            this.cbIzplashtane = new System.Windows.Forms.CheckBox();
+            this.lbCena = new System.Windows.Forms.Label();
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.lbCharact = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbBroi = new System.Windows.Forms.TextBox();
+            this.lbCenaIzplashtane = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblComputerName
+            // 
+            this.lblComputerName.AutoSize = true;
+            this.lblComputerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblComputerName.Location = new System.Drawing.Point(231, 9);
+            this.lblComputerName.Name = "lblComputerName";
+            this.lblComputerName.Size = new System.Drawing.Size(200, 20);
+            this.lblComputerName.TabIndex = 1;
+            this.lblComputerName.Text = "ASUS G750JZ-T4039D";
+            // 
+            // cbWindows
+            // 
+            this.cbWindows.AutoSize = true;
+            this.cbWindows.Location = new System.Drawing.Point(478, 85);
+            this.cbWindows.Name = "cbWindows";
+            this.cbWindows.Size = new System.Drawing.Size(193, 21);
+            this.cbWindows.TabIndex = 6;
+            this.cbWindows.Text = "OC:Windows 8.1 +105 лв.";
+            this.cbWindows.UseVisualStyleBackColor = true;
+            this.cbWindows.CheckedChanged += new System.EventHandler(this.cbWindows_CheckedChanged);
+            // 
+            // cbBackpack
+            // 
+            this.cbBackpack.AutoSize = true;
+            this.cbBackpack.Location = new System.Drawing.Point(477, 131);
+            this.cbBackpack.Name = "cbBackpack";
+            this.cbBackpack.Size = new System.Drawing.Size(159, 21);
+            this.cbBackpack.TabIndex = 7;
+            this.cbBackpack.Text = "Раница ASUS+45лв";
+            this.cbBackpack.UseVisualStyleBackColor = true;
+            this.cbBackpack.CheckedChanged += new System.EventHandler(this.cbBackpack_CheckedChanged);
+            // 
+            // cbIzplashtane
+            // 
+            this.cbIzplashtane.AutoSize = true;
+            this.cbIzplashtane.Location = new System.Drawing.Point(477, 173);
+            this.cbIzplashtane.Name = "cbIzplashtane";
+            this.cbIzplashtane.Size = new System.Drawing.Size(210, 21);
+            this.cbIzplashtane.TabIndex = 8;
+            this.cbIzplashtane.Text = "На изплащане 12мес.+20%";
+            this.cbIzplashtane.UseVisualStyleBackColor = true;
+            this.cbIzplashtane.CheckedChanged += new System.EventHandler(this.cbIzplashtane_CheckedChanged);
+            // 
+            // lbCena
+            // 
+            this.lbCena.AutoSize = true;
+            this.lbCena.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCena.Location = new System.Drawing.Point(494, 258);
+            this.lbCena.Name = "lbCena";
+            this.lbCena.Size = new System.Drawing.Size(0, 24);
+            this.lbCena.TabIndex = 9;
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBuy.Location = new System.Drawing.Point(472, 361);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(110, 48);
+            this.btnBuy.TabIndex = 11;
+            this.btnBuy.Text = "Купи";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // lbCharact
+            // 
+            this.lbCharact.AutoSize = true;
+            this.lbCharact.Location = new System.Drawing.Point(126, 415);
+            this.lbCharact.Name = "lbCharact";
+            this.lbCharact.Size = new System.Drawing.Size(224, 68);
+            this.lbCharact.TabIndex = 12;
+            this.lbCharact.Text = "Processor:Intel Core i7 Quad Core\r\nRAM:8GB\r\nDisplay:17.6\'\r\nVideo card:NIVIDIA GFo" +
+    "rce 720";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(469, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Брой";
+            // 
+            // tbBroi
+            // 
+            this.tbBroi.Location = new System.Drawing.Point(532, 213);
+            this.tbBroi.Name = "tbBroi";
+            this.tbBroi.Size = new System.Drawing.Size(100, 22);
+            this.tbBroi.TabIndex = 14;
+            this.tbBroi.TextChanged += new System.EventHandler(this.tbBroi_TextChanged);
+            // 
+            // lbCenaIzplashtane
+            // 
+            this.lbCenaIzplashtane.AutoSize = true;
+            this.lbCenaIzplashtane.Location = new System.Drawing.Point(495, 304);
+            this.lbCenaIzplashtane.Name = "lbCenaIzplashtane";
+            this.lbCenaIzplashtane.Size = new System.Drawing.Size(0, 17);
+            this.lbCenaIzplashtane.TabIndex = 15;
+            this.lbCenaIzplashtane.Click += new System.EventHandler(this.lbCenaIzplashtane_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::_2101681070_VeselaApostolova_domashna_rabota.Zad2.Properties.Resources.auss360;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 294);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 57);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_MouseHover);
+            this.pictureBox5.MouseHover += new System.EventHandler(this.pictureBox5_MouseHover);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::_2101681070_VeselaApostolova_domashna_rabota.Zad2.Properties.Resources.asusDisc;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 227);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_MouseHover);
+            this.pictureBox4.MouseHover += new System.EventHandler(this.pictureBox4_MouseHover);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::_2101681070_VeselaApostolova_domashna_rabota.Zad2.Properties.Resources.asus;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 144);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_MouseHover);
+            this.pictureBox3.MouseHover += new System.EventHandler(this.pictureBox3_MouseHover);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_2101681070_VeselaApostolova_domashna_rabota.Zad2.Properties.Resources.asusFront;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_MouseHover);
+            this.pictureBox2.MouseHover += new System.EventHandler(this.pictureBox2_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(129, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(334, 287);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(444, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Минете с мишката върху изображението,което искате да видите.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(550, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Добавете:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(125, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Характеристики:";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.Location = new System.Drawing.Point(472, 415);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(110, 68);
+            this.btnReset.TabIndex = 19;
+            this.btnReset.Text = "Изтрий поръчка";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(471, 489);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 41);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Изход:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 557);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbCenaIzplashtane);
+            this.Controls.Add(this.tbBroi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbCharact);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.lbCena);
+            this.Controls.Add(this.cbIzplashtane);
+            this.Controls.Add(this.cbBackpack);
+            this.Controls.Add(this.cbWindows);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblComputerName);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Магазин за компютри";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblComputerName;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.CheckBox cbWindows;
+        private System.Windows.Forms.CheckBox cbBackpack;
+        private System.Windows.Forms.CheckBox cbIzplashtane;
+        private System.Windows.Forms.Label lbCena;
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.Label lbCharact;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbBroi;
+        private System.Windows.Forms.Label lbCenaIzplashtane;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ laptop/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2101681070_VeselaApostolova_domashna_rabota.Zad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pictureBox2.Image;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pictureBox3.Image;
+        }
+
+        private void pictureBox4_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pictureBox4.Image;
+        }
+
+        private void pictureBox5_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pictureBox5.Image;
+        }
+        float backpack = 45;
+        float windows = 105;
+        double izplashtane = 0.20;
+        double basePrice = 2986.8;
+        double total = 0;
+
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                total = basePrice * double.Parse(tbBroi.Text);
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show("Enter quantity!!!");
+            }
+            MessageBox.Show($"Вие успешно закупихте лаптоп ASUS G750JZ-T4039D \nЦена: {Math.Round(basePrice*double.Parse(tbBroi.Text),0)}лв","Информация",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void cbWindows_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbWindows.CheckState == CheckState.Checked)
+            {
+                 basePrice += windows;
+                lbCena.Text = Math.Round(total + basePrice,0) + "лв";
+                lbCenaIzplashtane.Text = "Цена на лизинг:12x0лв";
+                lbCharact.Text = "OC Windows 8.1 \nProcessor:Intel Core i7 Quad Core \nRAM: 8GB  \nDisplay:17.6' \nVideo card:NIVIDIA GForce 720";
+            }
+        }
+
+        private void cbBackpack_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbBackpack.CheckState == CheckState.Checked)
+            {
+                basePrice+= backpack;
+                lbCena.Text =Math.Round( total + basePrice,0) + "лв";
+                lbCenaIzplashtane.Text = "Цена на лизинг:12x0лв";
+            }
+        }
+
+        private void cbIzplashtane_CheckedChanged(object sender, EventArgs e)
+        {
+            double rezultatIzplashtane = 0;
+            if (cbIzplashtane.CheckState == CheckState.Checked)
+            {
+                rezultatIzplashtane= (basePrice/12)+(basePrice*izplashtane);
+                lbCena.Text = Math.Round(total+basePrice)+"лв";
+                lbCenaIzplashtane.Text = "Цена на лизинг:12X " + Math.Round(rezultatIzplashtane,0) + "лв.";
+            }
+        }
+
+        private void lbCenaIzplashtane_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbBroi_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Искате ли да изтриете поръчката си?", "Изтриване на поръчка",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            cbBackpack.CheckState = CheckState.Unchecked;
+            cbIzplashtane.CheckState = CheckState.Unchecked;
+            cbWindows.CheckState = CheckState.Unchecked;
+            lbCena.Text = string.Empty;
+            lbCenaIzplashtane.Text = string.Empty;
+            tbBroi.Text = string.Empty;
+        }
+    }
+}
